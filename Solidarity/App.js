b44da61e613b2abaf1fc545f87f57be7582356a4
Jefferson De Andrade <jeffersonandrade1999@gmail.com>
2023-05-25 18:30:22 -0300

    First commit
    Primeiro commit do projeto

---
+++ Solidarity/App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+
+
+import { View, Text } from 'react-native';
+
+
+const App: () => Node = () => {
+  return (
+    <View>
+      <Text>
+        Testando
+      </Text>
+    </View>
+  );
+};
+
+export default App;
