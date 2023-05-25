d5796caa3339dae9676bd08c2908d932fee8e958
Jefferson De Andrade <jeffersonandrade1999@gmail.com>
2023-05-25 20:35:18 -0300

    criando as primeiras navegações

--- Solidarity/App.js
+++ Solidarity/App.js
@@ -9,17 +9,19 @@
 import React from 'react';
 import type {Node} from 'react';
 
-
 import { View, Text } from 'react-native';
+import { cadastro } from './components/Screens/Cadastro';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-
+const Stack = createNativeStackNavigator(); 
 const App: () => Node = () => {
   return (
-    <View>
-      <Text>
-        Testando
-      </Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Cadastro' component={cadastro}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
---
+++ Solidarity/components/Screens/Cadastro.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View,Text } from "react-native";
+
+
+const cadastro = () => {
+    return(
+        <View>
+            <Text>
+                Tela de cadastro
+            </Text>
+        </View>
+    )
+}
+
+export {cadastro};
